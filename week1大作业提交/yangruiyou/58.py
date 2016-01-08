5cd163700e31d792009c341263be8a73a4b7f5b8
yangruiyou85 <ruiyou.yang@gmail.com>
2016-01-09 00:40:48 +0800

    58同城爬取第一周大作业
    yangruiyou

--- week1大作业提交/yangruiyou/58.py
+++ week1大作业提交/yangruiyou/58.py
@@ -1,4 +1,5 @@
-#-*- coding=utf-8 -*-
+#!/usr/bin/python
+#-*- coding:utf-8 -*-
 from bs4 import BeautifulSoup
 import requests
 import time
@@ -8,39 +9,34 @@ url='http://bj.58.com/pbdn/?PGTID=0d100000-0000-1121-f41b-137aeef068b7&ClickID=6
 wb_data=requests.get(url)
 
 soup=BeautifulSoup(wb_data.text,'lxml')
-#print (soup)
-#pro_title=soup.select('.tbody.tr:nth-child > td.t')
-#pro_title=soup.select('.t')
-pro_title=soup.select('.t > a[target="_blank"]')[0].text
-print (pro_title)
-
-#print (pro_title)
-#print (pro_title)
-view_count=soup.select('ul.mtit_con_left.fl > li.count')
-note_time=soup.select('ul.mtit_con_left.fl > li.time')
-#pro_price=soup.select('div.person_add_top.no_ident_top > div.per_ad_left > div.col_sub.sumary > ul > li:nth-child')
-pro_price=soup.select('div.su_con>span.price')
-bug_type=soup.select('#divContacter > ul > ul')
-area=soup.select('tbody > tr:nth-child > td.t')
-pro_type=soup.select('#infolist > div.filterbar > table > tbody > tr > td:nth-child > div > h1')
-print (pro_title,view_count,note_time,pro_price,bug_type,area,pro_type)
-
-print ('杨瑞友')
-
-for pro_title,view_count,note_time,pro_price,bug_type,area,pro_type in zip(pro_title,view_count,note_time,pro_price,bug_type,area,pro_type):
-  data={
-  'pro_title':pro_title.get_text(),
-  'view_count':view_count.get_text(),
-  'note_time':note_time.get_text(),
-  'pro_price':pro_price.get_text(),
-  'bug_type':bug_type.get_text(),
-  'area':area.get_text(),
-  'pro_type':pro_type.get_text()
-
-  }
-  print (data)
+false_addr=soup.select('.t > a[target="_blank"]')
+real_addr1 = [addr.get('href')  for addr in false_addr]
+#print real_addr1
+real_addr =[ addr1 for addr1 in real_addr1 if addr1[0:16]=='http://bj.58.com']
+#real_addr = [addr.get('href')  for addr in false_addr if addr.startswith(addr,1,16)=='http://bj.58.com')]
+#print real_addr
 
 
-'''
-老师先提交下，作业是没完成。周末我只能好好研究下。
-'''
+for address in real_addr:
+  web_data=requests.get(address)
+  soup1=BeautifulSoup(web_data.text,'lxml')
+  pro_title=soup1.select('div.person_add_top.no_ident_top > div.per_ad_left > div.col_sub.mainTitle > h1')
+  view_count=soup1.select('ul.mtit_con_left.fl > li.count')
+  note_time=soup1.select('ul.mtit_con_left.fl > li.time')
+  pro_price=soup1.select('div.person_add_top.no_ident_top > div.per_ad_left > div.col_sub.sumary > ul > li > div.su_con > span')
+  bug_type=soup1.select('div.person_add_top.no_ident_top > div.per_ad_left > div.col_sub.sumary > ul > li > div.su_tit')
+  area=soup1.select('div.person_add_top.no_ident_top > div.per_ad_left > div.col_sub.sumary > ul > li > div.su_tit')
+
+
+
+
+  #print (pro_title,view_count,note_time,pro_price,bug_type,area)
+  for pro_title,view_count,note_time,pro_price,bug_type,area in zip(pro_title,view_count,note_time,pro_price,bug_type,area):
+    data={
+      'pro_title':pro_title.get_text(),
+      'view_count':view_count.get_text(),
+      'note_time':note_time.get_text(),
+      'pro_price':pro_price.get_text(),
+      'bug_type':bug_type.get_text(),
+      'area':area.get_text()}
+    print (data)
